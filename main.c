2c691aecc3d9301e2291e3820a15dcc88ccb1887
Alienxbe <martin.ykman@gmail.com>
2021-11-03 15:30:18 +0100

    Fixing norm errors

--- main.c
+++ main.c
@@ -6,18 +6,13 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 21:21:24 by mykman            #+#    #+#             */
-/*   Updated: 2021/11/02 09:53:20 by mykman           ###   ########.fr       */
+/*   Updated: 2021/11/03 15:29:36 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
 
-int main(void)
+int	main(void)
 {
-	char	*str = "0123 ";
-	char	*n = "0123456789";
-
-	ft_printf("ft_isnumber(%s): \"%d\"\n", str, ft_isnumber(str));
-	ft_printf("ft_isnumber(%s): \"%d\"\n", n, ft_isnumber(n));
 	return (0);
 }
