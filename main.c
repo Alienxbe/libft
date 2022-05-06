857c438ed08ef96cd63e5a0dfcbd35ddbfc3fafd
Alienxbe <martin.ykman@gmail.com>
2022-05-06 23:56:58 +0200

    Added printf-v2 to the lib + Added some more functions from libft-v2 utils + Need to comment all main functions + Need to Norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maykman <maykman@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 21:21:24 by mykman            #+#    #+#             */
-/*   Updated: 2022/05/06 02:01:14 by maykman          ###   ########.fr       */
+/*   Updated: 2022/05/06 23:55:10 by maykman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	main(void)
 {
-	char	*s = "Ceci est une chaine de charactere";
-	ft_print_memory_fd(s, 420, 1);
+	ft_fprintf(STDERR_FILENO, "Bonjour a vous %s %s!\n", "M.", "Eric");
 	return (0);
 }
